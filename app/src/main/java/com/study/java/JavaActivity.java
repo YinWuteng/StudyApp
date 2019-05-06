c8661982160e1849f2bda616bf5fb49914fcacce
Administrator <470047349@qq,com>
2019-05-06 21:29:42 +0800

    上传配置资源文件

--- app/src/main/java/com/study/java/JavaActivity.java
+++ app/src/main/java/com/study/java/JavaActivity.java
@@ -3,10 +3,14 @@ package com.study.java;
 import com.study.R;
 import com.study.base.BaseActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Java基础
  */
 public class JavaActivity extends BaseActivity {
+
     @Override
     protected int getLayoutId() {
         return R.layout.activity_java;
