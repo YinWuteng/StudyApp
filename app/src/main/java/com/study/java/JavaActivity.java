25505e00d9d25aeb86b9700dd72d2819a75fcff0
Administrator <470047349@qq,com>
2019-05-06 22:15:30 +0800

    java知识

--- app/src/main/java/com/study/java/JavaActivity.java
+++ app/src/main/java/com/study/java/JavaActivity.java
@@ -1,15 +1,17 @@
 package com.study.java;
 
-import com.study.R;
-import com.study.base.BaseActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.study.R;
+import com.study.algorithm.AlgorithmWebActivity;
+import com.study.base.BaseActivity;
 
 /**
  * Java基础
  */
-public class JavaActivity extends BaseActivity {
+public class JavaActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected int getLayoutId() {
@@ -18,6 +20,44 @@ public class JavaActivity extends BaseActivity {
 
     @Override
     protected void findId() {
+        Button btnJavaBase = findViewById(R.id.btn_java_base);
+        Button btnJavaDeepSource = findViewById(R.id.btn_deep_source);
+        Button btnDataStructure = findViewById(R.id.btn_data_structe);
+        Button btnThreadPool = findViewById(R.id.btn_thread_pool);
 
+        btnJavaBase.setOnClickListener(this);
+        btnJavaDeepSource.setOnClickListener(this);
+        btnDataStructure.setOnClickListener(this);
+        btnThreadPool.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent=new Intent();
+        switch (v.getId()) {
+
+            case R.id.btn_java_base: //java基础
+                intent.setClass(this, AlgorithmWebActivity.class);
+                intent.putExtra("url","https://github.com/YinWuteng/StudyApp/blob/master/app/src/main/java/com/study/java_base");
+                startActivity(intent);
+                break;
+            case R.id.btn_deep_source: //java深入源码
+                intent.setClass(this, AlgorithmWebActivity.class);
+                intent.putExtra("url","https://github.com/YinWuteng/StudyApp/blob/master/app/src/main/java/com/study/java_depe_source");
+                startActivity(intent);
+                break;
+            case R.id.btn_data_structe: //java数据结构
+                intent.setClass(this, AlgorithmWebActivity.class);
+                intent.putExtra("url","https://github.com/YinWuteng/StudyApp/blob/master/app/src/main/java/com/study/data_structure");
+                startActivity(intent);
+                break;
+            case R.id.btn_thread_pool: //java线程
+                intent.setClass(this, AlgorithmWebActivity.class);
+                intent.putExtra("url","https://github.com/YinWuteng/StudyApp/blob/master/app/src/main/java/com/study/thread_pool");
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
